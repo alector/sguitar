f31c07ffd5e81fe0cf026bdc26d4562470cd3de0
alector <panagiotispp@gmail.com>
2020-07-27 21:31:44 +0300

    Install react-headroom react plugin to create animated sticky header.

--- src/pages/demo.js
+++ src/pages/demo.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { AnchorLink } from "gatsby-plugin-anchor-links";
+import Headroom from "react-headroom"
 
 import { Link } from "gatsby"
 
@@ -10,19 +11,21 @@ const DemoPage = () => (
   <div>
 
     <section className="flex flex-col  h-screen   bg-black">
-      <header className="h-20 flex items-center  header bg-black w-full text-white">
-        <div className="logo ml-5 flex-grow ">ΣΩΤΗΡΗΣ </div>
-        <nav className="flex mr-5 uppercase">
-          <AnchorLink to="/demo#blog" title="Our team" stripHash><span>Blog</span></AnchorLink>
-          <a className="px-2" href="http://www.google.com">Projects</a>
-          <a className="px-2" href="http://www.google.com">Listen</a>
-          <a className="px-2" href="http://www.google.com">Listen</a>
 
-        </nav>
+      <Headroom>
+        <div className="h-20 flex items-center  header bg-black w-full text-white">
+          <div className="logo ml-5 flex-grow ">ΣΩΤΗΡΗΣ </div>
+          <nav className="flex mr-5 uppercase">
+            <AnchorLink className="px-2" to="/demo#blog" title="Our team" stripHash><span>Blog</span></AnchorLink>
+            <AnchorLink className="px-2" to="/demo#projects" title="Our team" stripHash><span>Projects</span></AnchorLink>
+            <a className="px-2" href="http://www.google.com">Listen</a>
+            <a className="px-2" href="http://www.google.com">Listen</a>
+          </nav>
 
-      </header>
+        </div>
+      </Headroom>
 
-      <div className="h-full flex flex-col justify-center items-center top-content bg-green-500 ">
+    <div className="h-full flex flex-col justify-center items-center top-content bg-green-500 ">
 
         <div className="flex flex-col w-200">
            <h2 className="text-center text-white text-2xl">Section 1</h2>
@@ -46,12 +49,12 @@ const DemoPage = () => (
       </div>
     </section>
 
-    <section className="flex flex-col  h-screen   bg-red-300">
+    <section id="projects" className="flex flex-col  h-screen   bg-red-300">
 
       <div className="h-full flex flex-col justify-center items-center top-content bg-red-500 ">
 
         <div className="flex flex-col w-200">
-          <h2 className="text-center text-white text-2xl">Section 3</h2>
+          <h2 className="text-center text-white text-2xl">PROJECTS</h2>
           <p className="text-center text-green-200">subtitle</p>
         </div>
 
